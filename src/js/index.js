9830ccac3d4d0816a699b60b08e4d506d15fa976
Salvador Montero <salvadormdev@gmail.com>
2024-08-28 10:46:32 -0400

    section promociones, map y footer completado

--- src/js/index.js
+++ src/js/index.js
@@ -113,146 +113,44 @@ window.sr = ScrollReveal();
     distance: '-300px'
   });
 
-  sr.reveal('.destinos .titulo', {
-    duration: 1500,
-    origin: 'rigth',
-    distance: '-250px'
-  });
-
-  sr.reveal('.destinos > h3', {
-    duration: 1500,
-    origin: 'rigth',
-    distance: '-250px'
-  });
-
-  sr.reveal('.destinos .imagenes', {
+  sr.reveal('.services .card-6', {
     duration: 3500,
     origin: 'top',
     distance: '-300px'
   });
 
-  sr.reveal('.paquetes .titulo', {
-    duration: 1500,
-    origin: 'rigth',
-    distance: '-250px'
-  });
-
-  sr.reveal('.paquetes > h3', {
-    duration: 1500,
-    origin: 'rigth',
-    distance: '-250px'
-  });
-
-  sr.reveal('.paquetes .card1', {
-    duration: 2000,
-    origin: 'top',
-    distance: '-300px'
-  });
-  
-  sr.reveal('.paquetes .card2', {
-    duration: 2500,
-    origin: 'top',
-    distance: '-350px'
-  });
-
-  sr.reveal('.paquetes .card3', {
-    duration: 3000,
-    origin: 'top',
-    distance: '-400px'
-  });
-
-  sr.reveal('.process .titulo', {
-    duration: 1500,
-    origin: 'rigth',
-    distance: '-250px'
-  });
-
-  sr.reveal('.process > h3', {
-    duration: 1500,
-    origin: 'rigth',
-    distance: '-250px'
-  });
-
-  sr.reveal('.process .card1', {
-    duration: 2000,
-    origin: 'top',
-    distance: '-300px'
-  });
-  
-  sr.reveal('.process .card2', {
-    duration: 2500,
-    origin: 'top',
-    distance: '-350px'
-  });
-
-  sr.reveal('.process .card3', {
-    duration: 3000,
-    origin: 'top',
-    distance: '-400px'
-  });
-
-  sr.reveal('.travel-guide .titulo', {
-    duration: 1500,
-    origin: 'rigth',
-    distance: '-250px'
-  });
-
-  sr.reveal('.travel-guide > h3', {
-    duration: 1500,
-    origin: 'rigth',
-    distance: '-250px'
-  });
-
-  sr.reveal ('.travel-guide .card1', {
-    duration: 2000,
-    origin: 'top',
-    distance: '-300px'
-  });
-  
-  sr.reveal('.travel-guide .card2', {
-    duration: 2500,
-    origin: 'top',
-    distance: '-350px'
-  });
-
-  sr.reveal('.travel-guide .card3', {
-    duration: 3000,
-    origin: 'top',
-    distance: '-400px'
-  });
-
-  sr.reveal('.testimonials .titulo', {
-    duration: 1500,
-    origin: 'rigth',
-    distance: '-250px'
-  });
-
-  sr.reveal('.testimonials > h3', {
-    duration: 1500,
-    origin: 'rigth',
-    distance: '-250px'
-  });
-
-  sr.reveal ('.testimonials .card1', {
-    duration: 2000,
-    origin: 'rigth',
-    distance: '-300px'
-  });
-  
-  sr.reveal('.testimonials .card2', {
-    duration: 2500,
-    origin: 'righ',
-    distance: '-350px'
-  });
-
-  sr.reveal('.testimonials .card3', {
-    duration: 3000,
-    origin: 'rigth',
-    distance: '-400px'
-  });
-
-  sr.reveal('.travel-guide .card4', {
+  sr.reveal('.services .card-1 i', {
     duration: 3500,
-    origin: 'top',
-    distance: '-400px'
+    origin: 'bottom',
+    distance: '-300px'
+  });
+
+  sr.reveal('.services .card-2 i', {
+    duration: 3500,
+    origin: 'bottom',
+    distance: '-300px'
+  });
+
+  sr.reveal('.services .card-3 i', {
+    duration: 3500,
+    origin: 'bottom',
+    distance: '-300px'
+  });
+
+  sr.reveal('.services .card-4 i', {
+    duration: 3500,
+    origin: 'bottom',
+    distance: '-300px'
+  });
+
+  sr.reveal('.services .card-5 i', {
+    duration: 3500,
+    origin: 'bottom',
+    distance: '-300px'
+  });
+
+  sr.reveal('.services .card-6 i', {
+    duration: 3500,
+    origin: 'bottom',
+    distance: '-300px'
   });
